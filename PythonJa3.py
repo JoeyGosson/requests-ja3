f538640f194931930f4bc71510cb52c122da5077
DESKTOP-JAOKL0O\Administrator <joey_gosson@sina.com>
2021-09-06 16:16:48 +0800

    nothing

--- PythonJa3.py
+++ PythonJa3.py
@@ -7,14 +7,12 @@
 @Time   : 2021-09-05 10:07
 @Desc   : 
 """
-
 import random
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.ssl_ import create_urllib3_context
 
 ORIGIN_CIPHERS = ('ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+HIGH:'
                   'DH+HIGH:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+HIGH:RSA+3DES')
-
 
 class DESAdapter(HTTPAdapter):
     def __init__(self, *args, **kwargs):
@@ -37,12 +35,10 @@ class DESAdapter(HTTPAdapter):
         kwargs['ssl_context'] = context
         return super(DESAdapter, self).proxy_manager_for(*args, **kwargs)
 
-
 import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67'}
 s = requests.Session()
 s.headers.update(headers)
-
 
 for _ in range(5):
     s.proxies.update()
